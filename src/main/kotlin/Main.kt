0e3745996908b3f9a57e9a9c5fe25c713e41dff3
Robert Kosakowski <rkosakowski@egnyte.com>
2024-12-08 12:52:18 +0100

    implement day 8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
-import day7.day7Part1
-import day7.day7Part2
+
+import day8.day8Part1
+import day8.day8Part2
 
 
 fun main() {
-    println(day7Part1())
-    println(day7Part2())
+    println(day8Part1())
+    println(day8Part2())
 }
